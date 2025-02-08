8b0437da273f7f0640968186678b73007541e923
zht98 <70802119+zht98@users.noreply.github.com>
2025-02-08 18:55:28 +0800

    Update fetch_playlist_url.py

--- fetch_playlist_url.py
+++ fetch_playlist_url.py
@@ -2,7 +2,6 @@ import requests
 import re
 
 def fetch_playlist_url():
-    print("Fetching playlist URL...")
     idn = {
         "大连新闻综合": "tcb3IB5",
         "大连生活": "JzcFkF4",
@@ -17,23 +16,17 @@ def fetch_playlist_url():
     output_lines = []
     for channel_name, channel_id in idn.items():
         url = f"https://dlyapp.dltv.cn/apiv4.5/api/m3u8_notoken?channelid={channel_id}"
-        print(f"URL: {url}")
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
-            print(f"Failed to fetch URL for {channel_name}: {url}")
             continue
         info = response.text
-        print(f"Full API Response for {channel_name}: {info}")
         match = re.search(r'"address":"(.*?)"', info)
         if not match:
-            print(f"Failed to extract playlist URL for {channel_name} from response")
             continue
         playlist_url = match.group(1).replace('\\/', '/')
-        print(f"Playlist URL for {channel_name}: {playlist_url}")
         output_lines.append(f"{channel_name},{playlist_url}")
     with open("大连地方台.txt", "w") as file:
         file.write("\n".join(output_lines))
-    print("File written successfully")
 
 if __name__ == "__main__":
     fetch_playlist_url()

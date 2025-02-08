227b6703511493881f609d07275281af611e7d5e
zht98 <70802119+zht98@users.noreply.github.com>
2025-02-08 18:51:37 +0800

    Update fetch_playlist_url.py

--- fetch_playlist_url.py
+++ fetch_playlist_url.py
@@ -11,11 +11,14 @@ def fetch_playlist_url():
         "大连少儿": "q6tZ6Ba",
         "大连购物": "N4S4uAj",
     }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
     output_lines = []
     for channel_name, channel_id in idn.items():
         url = f"https://dlyapp.dltv.cn/apiv4.5/api/m3u8_notoken?channelid={channel_id}"
         print(f"URL: {url}")
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         if response.status_code != 200:
             print(f"Failed to fetch URL for {channel_name}: {url}")
             continue
